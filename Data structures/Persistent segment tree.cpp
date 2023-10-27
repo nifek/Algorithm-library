a0a788cf2341b6d075339da069ef7bca99868529
nifek <99556171+nifek@users.noreply.github.com>
2023-10-27 15:17:32 +0200

    Update Persistent segment tree.cpp

--- Data structures/Persistent segment tree.cpp
+++ Data structures/Persistent segment tree.cpp
@@ -30,15 +30,73 @@ typedef pair<ll, ll> pll;
 const ll mod = 1e9 + 7;
 const ll base = 1e6 + 9;
 const ll inf = 1e9;
-const int MAX = 3e5 + 42;
+const int MAX = 5e5 + 42;
 const int LG = 20;
 
 random_device rd;
 mt19937 gen(rd());
 uniform_int_distribution<ll> dis(1, inf);
 
-void solve() {
+int n;
+int sz = 0;
+ll t[2 * LG * MAX + 5];
+int lf[2 * LG * MAX + 5], rt[2 * LG * MAX + 5];
 
+int build(int l = 0, int r = n) {
+    if(r - l == 1) return sz;
+    int m = l + r >> 1;
+    int v = ++sz;
+    lf[v] = build(l, m); rt[v] = build(m, r);
+    t[v] = 0;
+    return v;
+}
+
+int update(int i, int x, int v, int l = 0, int r = n) {
+    if(r - l == 1) {
+        t[++sz] = t[v] + x;
+        return sz;
+    }
+    int m = l + r >> 1;
+    int ans = ++sz;
+    lf[ans] = lf[v]; rt[ans] = rt[v];
+    if(i < m) lf[ans] = update(i, x, lf[ans], l, m);
+    else rt[ans] = update(i, x, rt[ans], m, r);
+    t[ans] = t[lf[ans]] + t[rt[ans]];
+    return ans;
+}
+
+ll get(int l, int r, int v, int tl = 0, int tr = n) {
+    if(r <= tl || tr <= l) return 0;
+    if(l <= tl && tr <= r) return t[v];
+    int tm = tl + tr >> 1;
+    return get(l, r, lf[v], tl, tm) + get(l, r, rt[v], tm, tr);
+}
+
+void solve() {
+    int q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin >> i;
+    }
+    int root = 0;
+    for(int i = 0; i < n; i++) {
+        root = update(i, a[i], root);
+    }
+    while(q--) {
+        int t;
+        cin >> t;
+        if(t == 0) {
+            int i, x;
+            cin >> i >> x;
+            root = update(i, x, root);
+        }
+        else {
+            int l, r;
+            cin >> l >> r;
+            cout << get(l, r, root) << '\n';
+        }
+    }
 }
 
 signed main() {
