441a0e11468ceee1211dff79ba2bbf52e91fad2f
nifek <tema.savich.b@gmail.com>
2025-04-09 14:08:22 +0200

    Change template

---
+++ template.cpp
@@ -0,0 +1,62 @@
+#ifdef LOCAL
+    #include <bits/include_all.h>
+#else
+    #include <bits/stdc++.h>
+    #include <ext/pb_ds/assoc_container.hpp>
+    #include <ext/pb_ds/tree_policy.hpp>
+#endif
+
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int) (x).size())
+#define pb push_back
+#define mp make_pair
+#define int long long
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> inline bool umin(T &a, const T &b) { if(a > b) { a = b; return 1; } return 0; }
+template <typename T> inline bool umax(T &a, const T &b) { if(a < b) { a = b; return 1; } return 0; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const ll mod = 998244353;
+const ll base = 1e6 + 9;
+const ll inf = 1e18;
+const int MAX = 2e5 + 42;
+const int LG = 20;
+
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<ll> dis(1, inf);
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin >> i;
+    }
+
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int ttt = 1;
+    cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+}
