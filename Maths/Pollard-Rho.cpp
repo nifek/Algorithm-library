9583388f6f5e6788c6289cbade34d0e52003159b
nifek <tema.savich.b@gmail.com>
2025-04-09 14:51:34 +0200

    Update math template

--- Maths/Pollard-Rho.cpp
+++ Maths/Pollard-Rho.cpp
@@ -10,13 +10,8 @@
 #pragma GCC optimize ("Ofast")
 #pragma GCC optimize ("unroll-loops")
 
-#define f first
-#define s second
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) ((int) (x).size())
-#define pb push_back
-#define mp make_pair
 #define int long long
 
 using namespace std;
@@ -27,16 +22,12 @@ template <typename T> inline bool umin(T &a, const T &b) { if(a > b) { a = b; re
 template <typename T> inline bool umax(T &a, const T &b) { if(a < b) { a = b; return 1; } return 0; }
 
 typedef long long ll;
-typedef unsigned long long ull;
 typedef long double ld;
 typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
 
-const ll mod = 998244353;
-const ll base = 1e6 + 9;
+ll mod = 998244353;
 const ll inf = 1e18;
 const int MAX = 2e5 + 42;
-const int LG = 20;
 
 random_device rd;
 mt19937 gen(rd());
@@ -247,7 +238,7 @@ void solve() {
     ll n;
     cin >> n;
     vector<ll> f = factor(n);
-    cout << sz(f) << " "; for(auto i : f) cout << i << " "; cout << '\n';
+    cout << f.size() << " "; for(auto i : f) cout << i << " "; cout << '\n';
 }
 
 signed main() {
